5ab494d3f600ae07273e31249b105394eebacb85
akounalake <kounalaki.artemis@yahoo.com>
2026-06-26 18:51:24 +0300

    feat: add to user controller get user by uuid

--- src/main/java/gr/aueb/cf/eduapp/api/UserRestController.java
+++ src/main/java/gr/aueb/cf/eduapp/api/UserRestController.java
@@ -2,19 +2,20 @@ package gr.aueb.cf.eduapp.api;
 
 import gr.aueb.cf.eduapp.core.exception.EntityAlreadyExistsException;
 import gr.aueb.cf.eduapp.core.exception.EntityInvalidArgumentException;
+import gr.aueb.cf.eduapp.core.exception.EntityNotFoundException;
 import gr.aueb.cf.eduapp.dto.UserInsertDTO;
 import gr.aueb.cf.eduapp.dto.UserReadOnlyDTO;
+import gr.aueb.cf.eduapp.model.User;
 import gr.aueb.cf.eduapp.service.UserService;
 import gr.aueb.cf.eduapp.core.exception.ValidationException;
 
+import jakarta.servlet.Servlet;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.UUID;
@@ -36,9 +37,29 @@ public class UserRestController {
         }
         UserReadOnlyDTO userReadOnlyDTO = userService.saveUser(userInsertDTO);
 
-        URI location = URI.create("api/v1/users" + userReadOnlyDTO.uuid());
+//        URI location = URI.create("api/v1/users" + userReadOnlyDTO.uuid());
+//        return ResponseEntity
+//                .created(location)
+//                .body(userReadOnlyDTO);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{uuid")
+                .buildAndExpand(userReadOnlyDTO.uuid())
+                .toUri();
+
         return ResponseEntity
                 .created(location)
                 .body(userReadOnlyDTO);
+
+    }
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<UserReadOnlyDTO> getUserByUUID(@PathVariable UUID uuid)
+            throws EntityNotFoundException {
+
+        return ResponseEntity.ok(userService.getUserByUuidDeletedFalse(uuid));
+
+
     }
 }
