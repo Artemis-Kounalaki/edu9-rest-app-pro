ba43ac75cbb6934a090347c0282edbc9d3e97d0f
akounalake <kounalaki.artemis@yahoo.com>
2026-07-04 17:57:21 +0300

    feat: add request and response auth dto

---
+++ src/main/java/gr/aueb/cf/eduapp/dto/AuthenticationRequestDTO.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.eduapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record AuthenticationRequestDTO(@NotNull
+                                       String username,
+
+                                       @NotNull
+                                       String password) {
+}
