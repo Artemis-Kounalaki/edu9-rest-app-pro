dfa9b98833e835dab73ebca5947af04d4ca75e52
akounalake <kounalaki.artemis@yahoo.com>
2026-07-04 16:54:25 +0300

    chore: logging was added

--- src/main/java/gr/aueb/cf/eduapp/service/UserService.java
+++ src/main/java/gr/aueb/cf/eduapp/service/UserService.java
@@ -20,7 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
 @Service
-@Slf4j //TODO
+@Slf4j
 @RequiredArgsConstructor
 
 public class UserService implements IUserService{
