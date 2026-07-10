e83f968588577635cbd4cdf608da1aed7528fedd
akounalake <kounalaki.artemis@yahoo.com>
2026-07-10 15:20:43 +0300

    chore : add eligible report logic with @Async and runner logic

--- src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
+++ src/main/java/gr/aueb/cf/eduapp/repository/TeacherRepository.java
@@ -1,6 +1,7 @@
 package gr.aueb.cf.eduapp.repository;
 
 //import gr.aueb.cf.eduapp.dto.TeacherStatusReportView;
+import gr.aueb.cf.eduapp.dto.TeacherStatusReportView;
 import gr.aueb.cf.eduapp.model.Teacher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -30,25 +31,25 @@ public interface TeacherRepository extends JpaRepository<Teacher, Long>,
     boolean existsByUuidAndUser_Uuid(UUID teacherUuid, UUID userUuid);
 
 
-//    @Query(value = """
-//        SELECT
-//            r.name AS periochi,
-//            t.firstname AS onoma,
-//            t.lastname AS eponymo,
-//            pi.amka AS amka,
-//            t.vat AS afm,
-//            CASE WHEN t.deleted = 1 THEN 'ΔΙΕΓΡΑΜΜΕΝΟΣ' ELSE 'ΕΝΕΡΓΟΣ' END AS katastasi,
-//            CASE
-//                WHEN t.created_at > '2025-01-01' THEN 'ΝΕΟΣ'
-//                WHEN t.created_at > '2023-01-01' THEN 'ΜΕΣΑΙΟΣ'
-//                WHEN t.created_at > '2020-01-01' THEN 'ΕΜΠΕΙΡΟΣ'
-//                ELSE 'ΠΑΛΙΟΣ'
-//            END AS empeiria
-//        FROM teachers t
-//        JOIN personal_information pi ON t.personal_info_id = pi.id
-//        JOIN regions r ON t.region_id = r.id
-//        WHERE t.deleted = 0
-//        ORDER BY t.deleted DESC, r.name
-//        """, nativeQuery = true)
-//    List<TeacherStatusReportView> findAllTeachersReport();
+    @Query(value = """
+        SELECT
+            r.name AS periochi,
+            t.firstname AS onoma,
+            t.lastname AS eponymo,
+            pi.amka AS amka,
+            t.vat AS afm,
+            CASE WHEN t.deleted = 1 THEN 'ΔΙΕΓΡΑΜΜΕΝΟΣ' ELSE 'ΕΝΕΡΓΟΣ' END AS katastasi,
+            CASE
+                WHEN t.created_at > '2025-01-01' THEN 'ΝΕΟΣ'
+                WHEN t.created_at > '2023-01-01' THEN 'ΜΕΣΑΙΟΣ'
+                WHEN t.created_at > '2020-01-01' THEN 'ΕΜΠΕΙΡΟΣ'
+                ELSE 'ΠΑΛΙΟΣ'
+            END AS empeiria
+        FROM teachers t
+        JOIN personal_information pi ON t.personal_info_id = pi.id
+        JOIN regions r ON t.region_id = r.id
+        WHERE t.deleted = 0
+        ORDER BY t.deleted DESC, r.name
+        """, nativeQuery = true)
+    List<TeacherStatusReportView> findAllTeachersReport();
 }
